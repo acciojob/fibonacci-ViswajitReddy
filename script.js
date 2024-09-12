b635eaf5d5fe9b9ade7b01a803d9ac4722b3e2a3
ViswajitReddy <142040689+ViswajitReddy@users.noreply.github.com>
2024-09-12 22:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+// your code herfunction fibonacciSeries(n) {
+    let fibSeries = [0, 1];
+    
+    for (let i = 2; i < n; i++) {
+        fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
+    }
+    
+    return fibSeries;
+}
 }
 
 module.exports = fibonacci;
